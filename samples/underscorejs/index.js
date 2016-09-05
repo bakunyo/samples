f38f02e407a2f2c12edef258457da15ea6083aa8
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-09-06 01:38:54 +0900

    Use OOP style

--- samples/underscorejs/index.js
+++ samples/underscorejs/index.js
@@ -2,4 +2,6 @@ var _ = require('underscore');
 
 var array = [0, 1, 2, 3];
 
-_.each(array, function(i) { console.log(i); });
+// _.each(array, function(i) { console.log(i); });
+
+_(array).each(function(i) { console.log(i); });
