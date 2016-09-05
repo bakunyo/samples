b0e67dc2b18894d11b7189792bef2eaa297cbcf0
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-09-06 01:41:08 +0900

    Use chain

--- samples/underscorejs/index.js
+++ samples/underscorejs/index.js
@@ -4,4 +4,9 @@ var array = [0, 1, 2, 3];
 
 // _.each(array, function(i) { console.log(i); });
 
-_(array).each(function(i) { console.log(i); });
+// _(array).each(function(i) { console.log(i); });
+
+_.chain(array)
+  .map(function(i) { return i + 1; })
+  .filter(function(i) { return i % 2 === 0; })
+  .each(function(i) { console.log(i); });
