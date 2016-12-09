81a95ef185af46b081c29219d99c92c0c6d7b63b
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-12-10 05:15:35 +0900

    Add MyWebpackPlugin

--- samples/webpack/webpack.config.js
+++ samples/webpack/webpack.config.js
@@ -1,3 +1,26 @@
+class MyWebpackPlugin {
+  apply(compiler) {
+    debugger;
+    compiler.plugin('compile', (params) => {
+      console.log('compile開始時の処理')
+    })
+
+    compiler.plugin('compilation', (compilation, params) => {
+      // compilationもまたpluginメソッドを持つので、各イベントに処理を挿し込める
+      compilation.plugin('optimize', () => {
+        console.log('optimize(最適化)開始時の処理')
+      })
+    })
+
+    // 非同期(async)に実行されるイベントはcallbackを引数で受取り、適宜実行する
+    compiler.plugin('emit', (compilation, callback) => {
+      console.log('emit(アセット出力)開始時の処理')
+      callback()
+    })
+  }
+}
+
+
 module.exports = {
   entry: "./entry.js",
   output: {
@@ -8,5 +31,8 @@ module.exports = {
     loaders: [
       { test: /\.css$/, loader: "style!css" }
     ]
-  }
+  },
+  plugins: [
+    new MyWebpackPlugin()
+  ]
 };
