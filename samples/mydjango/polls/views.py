675833d848fff36583096f3d6eb13a020c1b4652
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2017-04-05 15:45:07 +0900

    Add results action

--- samples/mydjango/polls/views.py
+++ samples/mydjango/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.core.urlresolvers import reverse
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -15,8 +15,8 @@ def detail(request, question_id):
   return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
