a7bb9ccf0fbc1f30d5eae750918fc7c9621f151c
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2017-07-02 18:23:56 +0900

    Add elixir sample

---
+++ samples/elixir/sample_func.ex
@@ -0,0 +1,16 @@
+defmodule SampleFunc do
+  def hello(person) do
+    IO.puts "Hello, #{person}. My pid is #{inspect self()}."
+    receive do
+      message -> IO.puts "Message is #{message}."
+    end
+  end
+
+  def hello2(person) do
+    IO.puts "Hello, #{person}. My pid is #{inspect self()}."
+    receive do
+      message -> IO.puts "Message is #{message}."
+      hello2(person)
+    end
+  end
+end
